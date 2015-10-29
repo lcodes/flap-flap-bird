01631202355d6e5d50279f6c973561ea5392d069
Jeremie Pelletier <jeremie.pelletier@frimastudio.com>
2015-10-29 15:37:54 -0400

    removed unused dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject flap-flap-bird "0.1.0-SNAPSHOT"
   :description "Flap Flap Bird!"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismofeverything/penumbra "0.6.12"]
                  [quil "2.2.6"]])
